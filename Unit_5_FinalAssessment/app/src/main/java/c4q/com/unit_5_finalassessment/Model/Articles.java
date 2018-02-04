a7dadabcf8d02fac63d52d39f6ecb281dbd0d0c0
Ashley Joachim <ashleyjoachim@ac.c4q.nyc>
2018-02-04 15:17:27 -0500

    model classes added

---
+++ Unit_5_FinalAssessment/app/src/main/java/c4q/com/unit_5_finalassessment/Model/Articles.java
@@ -0,0 +1,12 @@
+package c4q.com.unit_5_finalassessment.Model;
+
+
+public class Articles {
+    private Source source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+}
---
+++ Unit_5_FinalAssessment/app/src/main/java/c4q/com/unit_5_finalassessment/Model/Source.java
@@ -0,0 +1,15 @@
+package c4q.com.unit_5_finalassessment.Model;
+
+
+public class Source {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
